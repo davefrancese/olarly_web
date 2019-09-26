5ba985841dc89fad5ae6eb212c850eab4a71b30d
johnvehr <vehr@johnvehr.com>
2019-09-26 11:34:45 -0600

    added assets

--- src/components/layouts/auth.js
+++ src/components/layouts/auth.js
@@ -1,11 +1,22 @@
 import React from "react"
 import {Col, Container} from 'reactstrap'
+import Main from "../main";
+import Olarly_logo from '../../assets/img/logos/olarlymain.png'
 
 const Auth = ({children}) => (
   <React.Fragment>
-    <Container>
-      {children}
+  <Main className="auth-landing d-flex w-100">
+    <Container className="d-flex flex-column">
+      <Row className="h-100">
+        <Col sm="10" md="8" lg="6" className="mx-auto d-table h-100">
+          <div className="d-table-cell align-middle">
+          <img src={Olarly_logo} className="auth-logo-img" />
+            {children}
+          </div>
+        </Col>
+      </Row>
     </Container>
+  </Main>
   </React.Fragment>
 )
 
