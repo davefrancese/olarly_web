a5dfc9c6a369859d40f11784dfaeb629f1aee33f
johnvehr <vehr@johnvehr.com>
2019-11-19 11:33:04 -0700

    restructuring

--- src/components/layouts/landing.js
+++ src/components/layouts/landing.js
@@ -2,39 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 // import {Col, Container, Row} from 'reactstrap'
 // import Navigation from "../shared/navigation";
-import Profile from "../users/profile";
-
-import "../style/landing.scss";
 
 class LandingLayout extends React.Component {
-  renderContent() {
-    return this.props.isRegistered ? (
-      <div>
-        <Profile />
-      </div>
-    ) : (
-      <div>
-        <p>User False</p>
-      </div>
-    );
-  }
 
   render() {
     return (
       <div className="Landing-Layout">
-        <main>{this.renderContent()}</main>
+        <main>Good thought but do what you did on routes</main>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state, props) => {
-  return {
-    user: state.user.user
-  };
-};
 
-export default connect(
-  mapStateToProps,
-  null
-)(LandingLayout);
+export default LandingLayout
