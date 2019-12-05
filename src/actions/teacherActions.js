12f771ee388fda83567fedbfba54ee2973a4e846
dfrancese <davefrancese@gmail.com>
2019-12-04 21:27:50 -0500

    progress bar needs fixes + endpoint url

--- src/actions/teacherActions.js
+++ src/actions/teacherActions.js
@@ -4,6 +4,7 @@ import { TEACHER_REGISTER } from "./actionTypes";
 const teacherPostUrl = "";
 
 export const registerTeacher = data => async dispatch => {
+  console.log("MADE IT TO ACTION. JOHN, PUT IN ENDPOINT URL");
   const res = await axios.post(teacherPostUrl, data);
   const json = await res.json();
   dispatch({ type: TEACHER_REGISTER, payload: json });
